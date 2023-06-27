bdae87bdd5a167bd2b231fba45a57fa7d547ee56
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-06-27 23:07:49 +0300

    add animate

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ document.getElementById('scrollSecondButton').addEventListener('click', function
     let target = document.getElementById('targetSpan');
     target.scrollIntoView({behavior: 'smooth'})
 })
-document.getElementById('scrollThirdButton').addEventListener('click', function (){
+document.getElementById('scrollThirdButton').addEventListener('click', function () {
     let target = document.getElementById('targetDiv');
     target.scrollIntoView({behavior: 'smooth'})
 })
-document.querySelector('a[href^="#"]').addEventListener('click', function(event) {
+document.querySelector('a[href^="#"]').addEventListener('click', function (event) {
     event.preventDefault();
 
     let targetId = this.getAttribute('href').substring(1);
     let target = document.getElementById(targetId);
 
     if (target) {
-        target.scrollIntoView({ behavior: 'smooth' });
+        target.scrollIntoView({behavior: 'smooth'});
     }
 });
+
+
 
